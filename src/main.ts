897ed6a502af97147c95f36be5c089221dff5635
fahrijal-alkuja <alkuja@unikarta.ac.id>
2023-01-07 18:07:37 +0800

    fest commit

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-const port = 5000;
+const port = 9000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
-  await app.listen(port, '0.0.0.0');
+  await app.listen(port);
   Logger.log(
     `Server Runing to port:${port} url: ${await app.getUrl()}`,
     'Bootstrap',
